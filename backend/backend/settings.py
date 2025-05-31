dcea085738c24b1006482c3548de7213a3a60149
ahmad <daloona26@gmail.com>
2025-05-31 15:21:16 +0200

    Add

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -129,7 +129,8 @@ USE_TZ = True
 STATIC_URL = 'static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
+DEBUG = False
+ALLOWED_HOSTS = ['daloon26.pythonanywhere.com']
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
